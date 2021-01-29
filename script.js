4a5b1a336dd1f9637a3ebf12a3a8d160bc6604b2
Ashab Hussan <46869907+Shadow-Chaser@users.noreply.github.com>
2021-01-30 00:51:19 +0600

    buttons fixed

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ document.getElementById('first-class-increase').addEventListener('click', functi
     const firstClassCount = parseInt(firstClassInput.value);
     const newFirstClassCount = firstClassCount + 1;
     firstClassInput.value = newFirstClassCount;
+    const firstClassTotal = newFirstClassCount * 150;
+    // calculateTotal();
+    
 })
 
 document.getElementById('first-class-decrease').addEventListener('click', function(){
@@ -14,6 +17,9 @@ document.getElementById('first-class-decrease').addEventListener('click', functi
     }
     
     firstClassInput.value = newFirstClassCount;
+    const firstClassTotal = newFirstClassCount * -150;
+
+
 })
 
 
@@ -23,6 +29,7 @@ document.getElementById('economy-class-increase').addEventListener('click', func
     const firstClassCount = parseInt(firstClassInput.value);
     const newFirstClassCount = firstClassCount + 1;
     firstClassInput.value = newFirstClassCount;
+    const economyClassTotal = newFirstClassCount * 100;
 })
 
 document.getElementById('economy-class-decrease').addEventListener('click', function(){
@@ -34,5 +41,14 @@ document.getElementById('economy-class-decrease').addEventListener('click', func
     }
     
     firstClassInput.value = newFirstClassCount;
+    const economyClassTotal = newFirstClassCount * -100;
+
 })
 
+// function calculateTotal(){
+//     const subTotalInput = document.getElementById('sub-total');
+//     const subTotalCount = parseInt(subTotalInput.innerText);
+//     subTotalCount = subTotalCount + currentTotal;
+//     console.log(subTotalCount);
+
+// }
